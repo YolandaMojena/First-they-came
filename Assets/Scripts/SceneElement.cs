6d754a594a935c1020a9dac8df2e3c98deea0c43
EtherealWhisper <yomowilce@gmail.com>
2017-03-06 17:03:42 +0100

    "Orificating" :baby_chick:

---
+++ Assets/Scripts/SceneElement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneElement : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+    Material spriteMaterial;
+    SpriteRenderer goldRenderer;
+    Vector2 pos;
+
+    float threshold;
+    const float FREQUENCY = 0.05f;
+    const float STEP = 0.025f;
+    
+
+    public bool Orificated = false;
+
+	// Use this for initialization
+	void Start () {
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteMaterial = spriteRenderer.material;
+        pos = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A) && !Orificated)
+        {
+            Orificated = true;
+            StartCoroutine("Orificate");
+        }	
+	}
+
+    IEnumerator Orificate()
+    {
+        threshold = spriteMaterial.GetFloat("_Threshold");
+        while (threshold < 1)
+        {
+            yield return new WaitForSecondsRealtime(FREQUENCY);
+            spriteMaterial.SetFloat("_Threshold", threshold + STEP);
+            threshold = spriteMaterial.GetFloat("_Threshold");
+        }
+    }
+}
