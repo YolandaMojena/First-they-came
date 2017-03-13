bd6788575b08143e3625debcf28bb7ecf9beb7c2
Pablo Rincón García <zetaku5@gmail.com>
2017-03-14 00:48:15 +0100

    :eggplant: Créditos nuevos + Tutorial
    (es la primera :eggplant: que pongo jo)

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -7,10 +7,13 @@ public class Intro : MonoBehaviour {
 
 	public Image jamImage;
 	public Image playersImage;
+	public Image tutorialImage;
 	public Image titleImage;
+
 
 	public bool jamTime = false;
 	public bool playersTime = false;
+	public bool tutorialTime = false;
 	public bool blackTime = false;
 	public bool titleTime = false;
 
@@ -33,7 +36,9 @@ public class Intro : MonoBehaviour {
 			Fade ("jam");
 		else if (checkAlpha (jamImage) <= 0.02f && playersTime) {
 			Fade ("players");
-		} else if (checkAlpha (playersImage) <= 0.02f && blackTime) {
+		} else if (checkAlpha (playersImage) <= 0.02f && tutorialTime) {
+			Fade ("tutorial");
+		}else if (checkAlpha (tutorialImage) <= 0.02f && blackTime) {
 			Fade ("black");
 		}
 		else if (checkAlpha(black) <= 0.02f && titleTime)
@@ -50,18 +55,24 @@ public class Intro : MonoBehaviour {
 			break;
 		case "players":
 			playersTime = false;
-			playersImage.CrossFadeAlpha (0f, fadeTime + 1f, false);
-			music.GetComponent<AudioSource> ().Play();
+			playersImage.CrossFadeAlpha (0f, fadeTime, false);
+			StartCoroutine ("WaitForContinuing");
+			break;
+		case "tutorial":
+			//tutorialTime = false;
+			tutorialImage.CrossFadeAlpha (0f, fadeTime + 1f, false);
 			StartCoroutine ("WaitForContinuing");
 			break;
 		case "black":
 			blackTime = false;
-			black.CrossFadeAlpha (0f, fadeTime - 1f, false);
+			tutorialImage.CrossFadeAlpha (0f, fadeTime + 1f, false);
+			music.GetComponent<AudioSource> ().Play();
+			black.CrossFadeAlpha (0f, 0.7f, false);
 			StartCoroutine ("WaitForContinuing");
 			break;
 		case "title":
 			titleTime = false;
-			titleImage.CrossFadeAlpha (0f, fadeTime - 0.5f, false);
+			titleImage.CrossFadeAlpha (0f, fadeTime, false);
 			break;
 		}
 	}
@@ -72,15 +83,22 @@ public class Intro : MonoBehaviour {
 	}
 
 	public IEnumerator WaitForContinuing(){
+
 		yield return new WaitForSeconds (3f);
 
 		if (i == 1)
 			jamTime = true;
-		else if (i == 2)
+		else if (i == 2) {
 			playersTime = true;
-		else if (i == 3)
+		} else if (i == 3) {
+			yield return new WaitForSeconds (8f);
+			tutorialTime = true;
+		}
+		else if (i == 4) {
+			tutorialTime = false;
 			blackTime = true;
-		else if (i == 4)
+		}
+		else if (i == 5)
 			titleTime = true;
 		i += 1;
 	}
@@ -89,5 +107,4 @@ public class Intro : MonoBehaviour {
 		yield return new WaitForSeconds (2);
 		jamTime = true;
 	}
-
 }
